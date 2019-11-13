d596513be800d23b94049a6ea83c0b7fad0c7257
ynwd <ynwd@users.noreply.github.com>
2019-11-14 05:40:40 +0700

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin} = require('clean-webpack-plugin')
-const ManifestPlugin = require('webpack-manifest-plugin');
+const { CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
       app: './src/index.js',
       print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
-    new ManifestPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
         title: 'Output Management'
@@ -18,5 +21,6 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
 };
